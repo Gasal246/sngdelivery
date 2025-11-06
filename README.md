50a16d13105be413792b2928462ffa71d68eb767
Gasal246 <gasalgasal246@gmail.com>
2025-11-07 00:42:57 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# 🚀 SNGDELIVERY
+**Delivery Application for Searchngo Delivery Staff**
+
+---
+
+## 🛠️ Tech Stack
+- **React Native**
+- **NativeWind**
+- **JavaScript**
+- **React Navigation / Native**
+
+---
+
+## 🌐 URL
+[👉 Muhammed Gasal](https://muhammedgasal.com)
+[👉 Click Here To Download](https://muhammedgasal.com)
---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+    api.cache(true);
+    return {
+      presets: [
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel",
+      ],
+      plugins: [
+        // 'react-native-reanimated/plugin', // if using Reanimated
+        'react-native-worklets/plugin'    // add this
+      ],
+    };
+  };
